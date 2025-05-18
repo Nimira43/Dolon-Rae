6c7ddb21de9e91a21e0a3681e71c816a21997d34
Nimira43 <n.rabone72@aol.com>
2025-05-18 17:30:55 +0100

    Removed the canvas floor logic

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -17,11 +17,6 @@ class Player {
     this.draw()
     this.position.x += this.velocity.x   
     this.position.y += this.velocity.y   
-    
-    if (this.position.y + this.height + this.velocity.y < canvas.height) {
-      this.velocity.y += gravity  
-    } else {
-      this.velocity.y = 0
-    }
+    this.velocity.y += gravity  
   }
 }
