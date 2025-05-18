c3ca81a5a64d6e74c1a12eccb424f03e005578e3
Nimira43 <n.rabone72@aol.com>
2025-05-18 16:20:47 +0100

    Placed Sprite class in its own file.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { Sprite } from './classes/Sprite.js'
+
 import { floorCollisions, platformCollisions } from '../js/data/collisions.js'
 console.log(platformCollisions)
 
@@ -8,7 +10,7 @@ for (let i = 0; i < floorCollisions.length; i += 36) {
 floorCollisions2D.forEach((row) => {
   row.forEach((symbol) => {
     if (symbol === 202) {
-      
+
     }
   })
 })
@@ -23,20 +25,7 @@ const scaledCanvas = {
 }
 const gravity = 0.5
 
-class Sprite {
-  constructor({ position, imageSrc }) {
-    this.position = position
-    this.image = new Image()
-    this.image.src = imageSrc
-  }
-  draw() {
-    if (!this.image) return
-    ctx.drawImage(this.image, this.position.x, this.position.y)
-  }
-  update() {
-    this.draw()
-  }
-}
+
 
 class Player {
   constructor(position) {
