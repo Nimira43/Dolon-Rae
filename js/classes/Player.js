d82356ae0b1f3caf40082d0afc8bfa2b5ef49654
Nimira43 <n.rabone72@aol.com>
2025-05-18 17:44:56 +0100

    Checking for left and right collision.

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -28,8 +28,10 @@ class Player {
       const collisionBlock = this.collisionBlocks[i]
       if (
         this.position.y + this.height >= collisionBlock.position.y &&
-        this.position.y <= collisionBlock.position.y + collisionBlock.height
-      
+        this.position.y <= collisionBlock.position.y + collisionBlock.height &&
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x
+        
       ) {
 
       }
