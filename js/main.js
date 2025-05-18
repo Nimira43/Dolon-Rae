a9b7938ca3a2bac81daa774e172c4e2dc1cb620a
Nimira43 <n.rabone72@aol.com>
2025-05-18 13:20:24 +0100

    Creating Sprite class

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ canvas.width = 1024
 canvas.height = 576
 const gravity = 0.5
 
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position
+    this.image = new Image()
+    this.image.src = imageSrc
+  }
+  draw() {
+    if (!this.image) return
+    ctx.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
 class Player {
   constructor(position) {
     this.position = position 
