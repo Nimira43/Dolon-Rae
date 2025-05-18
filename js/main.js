2df725ca977f6259ddbf6f5d3058068cbf9d39af
Nimira43 <n.rabone72@aol.com>
2025-05-18 11:44:56 +0100

    Moving player down.

--- js/main.js
+++ js/main.js
@@ -4,8 +4,15 @@ const ctx = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-ctx.fillStyle = '#fff'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+let y = 100
 
-ctx.fillStyle = '#ff4500'
-ctx.fillRect(200, 100, 100, 100)
+function animate() {
+  window.requestAnimationFrame(animate)
+  ctx.fillStyle = '#fff'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = '#ff4500'
+  ctx.fillRect(200, y, 100, 100)
+  y++
+}
+
+animate()
