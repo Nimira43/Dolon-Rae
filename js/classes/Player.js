dbc710805657a4f403885b4e68a26f3b21ec8a32
Nimira43 <n.rabone72@aol.com>
2025-05-18 17:53:30 +0100

    Moved collision check logic into its own utility file.

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -27,10 +27,7 @@ class Player {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
       const collisionBlock = this.collisionBlocks[i]
       if (
-        this.position.y + this.height >= collisionBlock.position.y &&
-        this.position.y <= collisionBlock.position.y + collisionBlock.height &&
-        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-        this.position.x + this.width >= collisionBlock.position.x
+        
         
       ) {
 
