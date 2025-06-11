32bc3b0957129e841f3468b80937c4c659d0b9c1
Nimira43 <n.rabone72@aol.com>
2025-06-11 08:24:04 +0100

    Bringing in animations prop into instance of Player - setting up Idle sprite

--- js/main.js
+++ js/main.js
@@ -57,7 +57,13 @@ const player = new Player({
   },
   collisionBlocks,
   imageSrc: '../images/warrior/Idle.png',
-  frameRate: 8
+  frameRate: 8,
+  animations: {
+    Idle: {
+      imageSrc: '../images/warrior/Idle.png',
+      frameRate: 8,
+    }
+  }
 })
 
 const background = new Sprite({
